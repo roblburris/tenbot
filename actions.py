192d54ee2f00a81149ef4076d10a0f160e1e4838
roblburris <roblburris@gmail.com>
2020-08-16 00:42:46 +0000

    finished tutorials

--- actions.py
+++ actions.py
@@ -1,31 +1,10 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
 from rasa_sdk.forms import FormAction
+from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List, Union
+from rasa_sdk import Tracker
+from rasa_sdk import Action
+from rasa_sdk.events import UserUtteranceReverted
+
 
 class SalesForm(FormAction):
     """Collects sales information and adds it to the spreadsheet"""
@@ -43,3 +22,33 @@ class SalesForm(FormAction):
             "company",
             "business_email",
         ]
+
+
+    def submit(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+        ) -> List[Dict]:
+
+        dispatcher.utter_message("Thanks for getting in touch, we’ll contact you soon")
+        return []
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict[Text, Any]]]]:
+        """A dictionary to map required slots to
+        - an extracted entity
+        - intent: value pairs
+        - a whole message
+        or a list of them, where a first match will be picked"""
+        return {"use_case": self.from_text(intent="inform")}
+
+
+class ActionGreetUser(Action):
+    """Revertible mapped action for utter_greet"""
+
+    def name(self):
+        return "action_greet"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_template("utter_greet", tracker)
+        return [UserUtteranceReverted()]
